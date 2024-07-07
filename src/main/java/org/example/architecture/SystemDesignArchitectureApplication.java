095d26a991548d5872bf5af724163c808abab6bb
SeokRae <kslbsh@gmail.com>
2024-07-07 23:50:54 +0900

    Initial commit

---
+++ src/main/java/org/example/architecture/SystemDesignArchitectureApplication.java
@@ -0,0 +1,13 @@
+package org.example.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesignArchitectureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SystemDesignArchitectureApplication.class, args);
+  }
+
+}
